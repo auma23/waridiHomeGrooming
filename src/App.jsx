1df54b5c8d5081a9e66f969c73e2f3dad58e7ce8
auma23 <dianaauma75@gmail.com>
2024-05-15 09:51:39 +0300

    update about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
+import AboutUs from './Components/AboutUs'
 import CoverSection from './Components/CoverSection'
 import HowItWorks from './Components/HowItWorks'
 import NavigationBar from './Components/NavigationBar'
+import PriceList from './Components/PriceList'
 import Services from './Components/Services'
+import Footer from './Components/Footer'
+import WhyChooseUs from './Components/WhyChooseUs'
+import TeamMember from './Components/TeamMember'
+import Contact from './Components/Contact'
+
 
 function App() {
 
@@ -12,6 +19,13 @@ function App() {
       <CoverSection />
       <HowItWorks />
       <Services />
+      <AboutUs />
+      <PriceList />
+      <WhyChooseUs />
+      <TeamMember />
+      <Contact />
+      <Footer />
+      
     </div>
     
   )
---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,49 @@
+
+// function AboutUs() {
+//     return (
+//       <div className="bg-white rounded-lg shadow-md overflow-hidden">
+//         <div className="flex flex-col md:flex-row md:h-screen">
+//           <img className="h-full w-full object-cover md:w-1/2" src="./src/assets/image3.jpg" alt="Wasafi Laundromat" />
+//           <div className="flex flex-col p-6 md:pl-12 md:w-1/2 white-space-nowrap text-left">  <h2 className="text-xl font-bold mb-2 text-green">Wasafi Laundromat</h2>
+//             <p className="text-orange text-base">Leave It And We Clean It</p>
+//             <p className="text-gray-700 text-base mt-4">
+//               Wasafi Laundromat is a newly established launderette owned and operated by Goldsands Limited. Wasafi Laundromat initially operates at the Seasons Residence Building, Kasarani and shall soon be available in other parts of the City. The Goldsands brand has a name for integrity, quality, and timely delivery.
+//             </p>
+//             <p className="text-orange text-base mt-3">What we offer</p>
+//             <p className="text-gray-700 text-base mt-4">
+//               Wasafi Laundromat is a newly established launderette owned and operated by Goldsands Limited. Wasafi Laundromat initially operates at the Seasons Residence Building, Kasarani and shall soon be available in other parts of the City. The Goldsands brand has a name for integrity, quality, and timely delivery.
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+  
+//   export default AboutUs
+  
+function AboutUs() {
+    return (
+      <div className="bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="text-center p-6 text-3xl font-bold text-green">
+          About Us
+        </div>
+        <div className="flex flex-col md:flex-row md:h-screen">
+          <img className="h-full w-full object-cover md:w-1/2" src="./src/assets/image3.jpg" alt="Waridi Home grooming services" />
+          <div className="flex flex-col p-6 md:pl-12 md:w-1/2 white-space-nowrap text-left">
+            <h2 className="text-2xl font-bold mb-2 text-green">Waridi Home Grooming Services</h2>
+            <p className="text-orange text-xl">Your Space, Transformed</p>
+            <p className="text-gray-700 text-xl mt-4">
+            Waridi Home Grooming Services is a Kenyan-based company dedicated to transforming your space into a haven of comfort and beauty. We offer a comprehensive range of services designed to meet all your interior design, decor, and cleaning needs for both residential and commercial.
+            </p>
+            <p className="text-orange text-xl mt-3">A Haven by Design</p>
+            <p className="text-gray-700 text-xl mt-4">
+            Our journey with you begins with a conception of your dream space. Whether you are starting from scratch or revamping an existing space, our experienced interior design team will collaborate with you to understand your vision, lifestyle, and budget. We offer initial consultations to delve into your desires, followed by detailed design plans that translate your dreams into reality.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default AboutUs
+  
